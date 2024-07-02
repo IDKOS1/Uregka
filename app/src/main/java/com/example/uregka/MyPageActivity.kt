f73d4f2b5e38b0754f02e91e3c26d77360594dc3
idkos <leegang31@gmail.com>
2024-07-02 14:05:55 +0900

    add:액티비티 생성

---
+++ app/src/main/java/com/example/uregka/MyPageActivity.kt
@@ -0,0 +1,16 @@
+package com.example.uregka
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MyPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_my_page)
+
+    }
+}
---
+++ app/src/main/java/com/example/uregka/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.example.uregka
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+    }
+}
