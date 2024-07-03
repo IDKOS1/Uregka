c1b2adc172b6a80d54e4090b045e4b573e4684c2
idkos <leegang31@gmail.com>
2024-07-03 17:43:36 +0900

    ➕Add: MainActivity 로직 추가

--- app/src/main/java/com/example/uregka/MainActivity.kt
+++ app/src/main/java/com/example/uregka/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.example.uregka
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main) }
+        setContentView(R.layout.activity_main)
+
+        var style = R.layout.item_follow_list
+        val followLayout = findViewById<LinearLayout>(R.id.ll_following_list)
+        for (follow in UserData.userList.values) {
+            val constraintLayout =
+                layoutInflater.inflate(style, followLayout, false)
+
+            follow.profileImg = "news"
+            val imageView = constraintLayout.findViewById<ImageView>(R.id.profile_image!!)
+            val resourceId = resources.getIdentifier(follow.profileImg, "drawable",packageName)
+            val textView = constraintLayout.findViewById<TextView>(R.id.profile_name!!)
+
+            imageView.setImageResource(resourceId)
+            textView.text = follow.userNickName
+
+            followLayout.addView(constraintLayout)
+        }
+    }
 }
