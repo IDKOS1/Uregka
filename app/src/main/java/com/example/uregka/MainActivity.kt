e740ba94536238ae43ac9ee35590143f1a12c580
idkos <leegang31@gmail.com>
2024-07-03 19:06:15 +0900

    🔨Refactor: 코드 수정

--- app/src/main/java/com/example/uregka/MainActivity.kt
+++ app/src/main/java/com/example/uregka/MainActivity.kt
@@ -14,13 +14,41 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var style = R.layout.item_follow_list
+        makeFollowList()
+        makeNewsList()
+
+
+
+    }
+
+    private fun makeFollowList() {
+        // 상단 팔로우 리스트 생성
+        val style = R.layout.item_follow_list
         val followLayout = findViewById<LinearLayout>(R.id.ll_following_list)
         for (follow in UserData.userList.values) {
             val constraintLayout =
                 layoutInflater.inflate(style, followLayout, false)
 
-            follow.profileImg = "news"
+            follow.profileImg = "sample"
+            val imageView = constraintLayout.findViewById<ImageView>(R.id.profile_image!!)
+            val resourceId = resources.getIdentifier(follow.profileImg, "drawable",packageName)
+            val textView = constraintLayout.findViewById<TextView>(R.id.profile_name!!)
+
+            imageView.setImageResource(resourceId)
+            textView.text = follow.userNickName
+
+            followLayout.addView(constraintLayout)
+        }
+    }
+
+    private fun makeNewsList() {
+        // 뉴스 메인 피드 생성
+        val style = R.layout.item_follow_list
+        val followLayout = findViewById<LinearLayout>(R.id.ll_following_list)
+        repeat(5) {
+            val constraintLayout =
+                layoutInflater.inflate(style, followLayout, false)
+
             val imageView = constraintLayout.findViewById<ImageView>(R.id.profile_image!!)
             val resourceId = resources.getIdentifier(follow.profileImg, "drawable",packageName)
             val textView = constraintLayout.findViewById<TextView>(R.id.profile_name!!)
