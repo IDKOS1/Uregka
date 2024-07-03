15587e8432f8cddfe4b1b01268fe9e08be625146
Jisung15 <gimseongjin302@gmail.com>
2024-07-03 17:14:49 +0900

    🔨Refactor : Activity 이름 수정

--- app/src/main/java/com/example/uregka/MainActivity.kt
+++ app/src/main/java/com/example/uregka/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
