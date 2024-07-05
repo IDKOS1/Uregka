660eb91986f00852cb31fcb0395292f2f655f6a9
GGo-ging <inki0126@naver.com>
2024-07-05 11:13:25 +0900

    💫 Feature : 스플래쉬 화면 제작

---
+++ app/src/main/java/com/example/uregka/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.uregka
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intnet = Intent(this, MainActivity::class.java)
+            startActivity(intnet)
+            finish()
+        }, 1500)
+
+    }
+}
