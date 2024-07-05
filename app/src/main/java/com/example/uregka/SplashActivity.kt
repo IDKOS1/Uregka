03b12463fdb91febbf9afcaffc2dad3f186bce3b
kdbswo <kdbswo@gmail.com>
2024-07-05 15:26:20 +0900

    🔨Refactor: import 제거

--- app/src/main/java/com/example/uregka/SplashActivity.kt
+++ app/src/main/java/com/example/uregka/SplashActivity.kt
@@ -6,8 +6,6 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
