61be4df30d8d84519cc843e88bd31a028dae7e07
idkos <leegang31@gmail.com>
2024-07-06 13:39:34 +0900

    🔨Refactor: 로직 추가

--- app/src/main/java/com/example/uregka/FollowingListActivity.kt
+++ app/src/main/java/com/example/uregka/FollowingListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uregka
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -13,24 +14,32 @@ class FollowingListActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_following_list)
 
-
-       /*
-        var item = R.layout.following_list_item
-
+        val item = R.layout.item_card_list
         val ll_following = findViewById<LinearLayout>(R.id.ll_following_list)
-        for (following in followingList) {
-            val constraintLayout =
+        for (following in UserData.userList.values) {
+            val layout =
                 layoutInflater.inflate(item, ll_following, false)
 
-            val imageView = constraintLayout.findViewById<ImageView>(R.id.poster!!)
-            val resourceId = resources.getIdentifier(following.imgSrc, "drawable",packageName)
-            val textView = constraintLayout.findViewById<TextView>(R.id.title!!)
+            val imageView = layout.findViewById<ImageView>(R.id.img_profile)
+            val resourceId = resources.getIdentifier(following.profileImg, "drawable",packageName)
+            val name = layout.findViewById<TextView>(R.id.tv_name)
+            val id = layout.findViewById<TextView>(R.id.tv_id)
+            val intro = layout.findViewById<TextView>(R.id.tv_intro)
 
             imageView.setImageResource(resourceId)
-            textView.text = following.title
+            name.text = following.userNickName
+            id.text = following.userId
+            intro.text = following.userIntro
 
-            ll_following.addView(constraintLayout)
+
+            ll_following.addView(layout)
         }
-        */
+
+        val back = findViewById<ImageView>(R.id.img_back)
+        back.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
