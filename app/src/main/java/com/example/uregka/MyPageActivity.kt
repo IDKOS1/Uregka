91f0a1f1e9738997eabb939d6925f37057c0f03f
idkos <leegang31@gmail.com>
2024-07-06 16:03:53 +0900

    🔨Refactor: 데이터 수신 추가

--- app/src/main/java/com/example/uregka/MyPageActivity.kt
+++ app/src/main/java/com/example/uregka/MyPageActivity.kt
@@ -1,12 +1,11 @@
 package com.example.uregka
 
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +13,46 @@ class MyPageActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_my_page)
 
+        val profileImg = findViewById<ImageView>(R.id.iv_profile_image)
         val followButton = findViewById<TextView>(R.id.tv_follow)
+        val id = findViewById<TextView>(R.id.tv_user_id)
+        val nickName = findViewById<TextView>(R.id.tv_user_nickname)
+        val follow = findViewById<TextView>(R.id.tv_user_followers)
+        val intro = findViewById<TextView>(R.id.tv_user_intro)
+        val email = findViewById<TextView>(R.id.tv_user_email)
 
-        followButton.setOnClickListener {
-            when (followButton.text) {
-                "Follow" -> followButton.text = "Unfollow"
-                "Unfollow" -> followButton.text = "Follow"
+        val user = intent.getParcelableExtra("userData", User::class.java)
+        val userName = intent.getStringExtra("userName")
+
+        if (user != null) {
+            if (userName.toString() in user.userNickName) {
+                val resourceId = resources.getIdentifier(user.profileImg, "drawable", packageName)
+                profileImg.setImageResource(resourceId)
+                id.text = user.userId
+                nickName.text = user.userNickName
+                follow.text = user.follower.toString()
+                intro.text = user.userIntro
+                email.text = user.userEmail
             }
         }
 
+        followButton.setOnClickListener {
+            var followCount = follow.text.toString().toInt()
+
+            when (followButton.text) {
+                "Follow" -> {
+                    followButton.text = "Unfollow"
+                    followButton.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.wrong))
+                    followCount++
+                    follow.text = followCount.toString()
+                }
+                "Unfollow" -> {
+                    followButton.text = "Follow"
+                    followButton.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.point))
+                    followCount--
+                    follow.text = followCount.toString()
+                }
+            }
+        }
     }
 }
